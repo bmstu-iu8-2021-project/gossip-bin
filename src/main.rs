4944a86a60255d558669ac2024939480e5d65f6a
zinstack625 <klimanov625@outlook.com>
2021-12-25 19:02:41 +0300

    final prettyfying

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,41 @@
-use gossip::whisper::Message;
+use env_logger;
+use gossip::whisper::{Message, MessageType};
 use gossip::{self, neighborhood};
 use std::net::ToSocketAddrs;
 use std::sync::mpsc;
 use std::thread;
 
-fn client(rx: mpsc::Receiver<Message>, tx: mpsc::Sender<Message>) {
+fn client(client_name: String, mut client_handle: gossip::config::ClientHandle) {
+    let mut sender_handle = client_handle.clone();
     let read_thread = thread::spawn(move || loop {
         let mut line = String::new();
         std::io::stdin()
             .read_line(&mut line)
             .expect("Failed to read stdin");
-        let new_msg = Message::new(
-            gossip::whisper::MessageType::Text,
-            &neighborhood::Node::with_address(
-                String::from("Zeus"),
-                0,
-                std::net::SocketAddr::new("0.0.0.0".parse().unwrap(), 0),
-            ),
-            &line,
-            vec![0],
-            0,
-            &vec![0; 12],
-            std::time::SystemTime::now(),
-        );
-        tx.send(new_msg)
-            .expect("Unable to communicate with server!");
+        let new_msg = Message::from_client(MessageType::Text, &client_name, &line);
+        sender_handle.send_msg(new_msg);
     });
 
     loop {
-        match rx.try_recv() {
+        match client_handle.get_msg() {
             Ok(msg) => {
                 println!("{}", msg.format());
             }
-            Err(mpsc::TryRecvError::Empty) => {
-                thread::sleep(std::time::Duration::from_millis(200));
-            }
-            Err(mpsc::TryRecvError::Disconnected) => {
-                break;
+            Err(_) => {
+                println!("Error happened!");
             }
         };
     }
-    read_thread.join().expect("Unable to join threads!");
 }
 
 fn main() {
+    env_logger::init();
     let init_node_list: Vec<String> = std::env::args().collect();
+    let mut client_name = None;
     let mut connect_node = Vec::<std::net::SocketAddr>::new();
     if init_node_list.len() > 1 {
-        for i in &init_node_list[1..] {
+        client_name = Some(init_node_list[1].to_string());
+        for i in &init_node_list[2..] {
             let mut uri = i
                 .to_socket_addrs()
                 .expect("Unable to parse initial connections URI's");
@@ -56,14 +44,13 @@ fn main() {
             connect_node.push(result);
         }
     }
+    let client_name = client_name.expect("No name given!");
     let config = gossip::config::Config {
         max_send_peers: 5,
         stored_messages_filename: String::from("gossip.db"),
     };
-    let (config_tx, config_rx) = mpsc::channel();
-    let (client_tx, client_rx) =
-        gossip::spawn_server(String::from("Aphrodite"), connect_node, config_rx);
-    config_tx.send(config);
-    let client_thread = thread::spawn(|| client(client_rx, client_tx));
+    let mut client_handle = gossip::spawn_server(client_name.clone(), connect_node);
+    client_handle.update_config(config);
+    let client_thread = thread::spawn(move || client(client_name, client_handle));
     client_thread.join().expect("Unable to join threads");
 }
